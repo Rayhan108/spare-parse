ccda3ee5f6f2a78a2c9d362f20e808e04c982532
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-05-04 14:54:36 +0600

    created single product page

---
+++ src/app/(with-layout)/product/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+const SingleProduct = () => {
+    return (
+        <div>
+            <div className=" container mx-auto py-20">
+                Single Product
+            </div>
+        </div>
+    );
+};
+
+export default SingleProduct;
